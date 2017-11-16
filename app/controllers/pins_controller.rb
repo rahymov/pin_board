740bf497c30ec2e750508a1d24331d4cb1e654e7
Yazgylych <y.rahymov@gmail.com>
2017-11-16 14:51:41 -0500

    Added user_id migration

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -3,11 +3,11 @@ class PinsController < ApplicationController
   before_action :find_pin, only: [:show, :edit, :update, :destroy]
 
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build
   end
 
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
     if @pin.save
       redirect_to @pin, notice: "Successfully created new Pin"
     else 
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,3 @@
 class Pin < ApplicationRecord
-	belong_to :user
+	belongs_to :user
 end
