b8f738a19c5366e0f19f3f83578f3fff0bd796f9
Yazgylych <y.rahymov@gmail.com>
2017-11-17 18:07:08 -0500

    Added to Edit New Show image_tag

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,7 @@
 class Pin < ApplicationRecord
+	
 	belongs_to :user
+
+	has_attached_file :image, styles: {medium: "400x400>"}
+	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/ 
 end
