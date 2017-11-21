72c37e144bd0a0b19f15c06896212b4935d8bc34
Yazgylych <y.rahymov@gmail.com>
2017-11-20 19:32:13 -0500

    Done styling

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,7 @@
 class PinsController < ApplicationController
   
   before_action :find_pin, only: [:show, :edit, :update, :destroy, :upvote]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def new
     @pin = current_user.pins.build
